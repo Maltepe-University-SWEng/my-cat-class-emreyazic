29585e0a2557c80335bb02ea94194223fee3727a
YusufEmreYazici <220704025@st.maltepe.edu.tr>
2025-03-04 15:49:25 +0300

    Cat class

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,17 +1,11 @@
-class Cat {
-    // Method definition
-    void meow() {
-        System.out.println("Meow!");
-    }
-}
+package tr.edu.maltepe.oop;
 
-public class Main {
+public class Simple_main {
     public static void main(String[] args) {
-        // Creating two objects of the Cat class
-        Cat cat1 = new Cat();
-        Cat cat2 = new Cat();
 
-        // Calling the meow method on both objects
+        Cat cat1 = new Cat("Emre", 7);
+        Cat cat2 = new Cat("Merve", 8);
+
         cat1.meow();
         cat2.meow();
     }
