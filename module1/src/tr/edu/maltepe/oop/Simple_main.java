604a92310d28d9ae77e31a62d21048840389d0ac
Yusuf Emre Yazıcı <64381132+emreyazic@users.noreply.github.com>
2025-03-04 15:13:24 +0300

    meow
    Also define a method called meow.
    
    Create 2 objects in the main program and call the method  meow.

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,46 +1,19 @@
 package tr.edu.maltepe.oop;
-public class Class {
-    private String name;
-    private int age;
-
-    // Constructor
-    public Class(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    // Getter for name
-    public String getName() {
-        return name;
-    }
-
-    // Setter for name
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Getter for age
-    public int getAge() {
-        return age;
-    }
-
-    // Setter for age
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    // meow method
-    public void meow() {
+class Cat {
+    // Method definition
+    void meow() {
         System.out.println("Meow!");
     }
+}
 
+public class Main {
     public static void main(String[] args) {
-        // Creating objects
-        Class obj1 = new Class("Kitty", 2);
-        Class obj2 = new Class("Tom", 3);
+        // Creating two objects of the Cat class
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
 
-        // Calling the meow method
-        obj1.meow();
-        obj2.meow();
+        // Calling the meow method on both objects
+        cat1.meow();
+        cat2.meow();
     }
 }
